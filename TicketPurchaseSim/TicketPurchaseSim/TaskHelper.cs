2efa63b9bbb2d4ef4964961fd0ca7c8cc7d501ac
Alok Mehrotra <Alok.Mehrotra@tattsgroup.com>
2019-10-11 09:41:43 +1000

    Added Ticket Purchase Sim

---
+++ TicketPurchaseSim/TicketPurchaseSim/TaskHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+
+namespace TicketPurchaseSim
+{
+    public static class Extensions
+    {
+        public static async Task RemoveCompletedTasks(this List<Task> tasks, CancellationToken token)
+        {
+            await Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Task[] tasksCopy = tasks.Where(t => t!=null && (t.IsCompleted || t.IsCanceled || t.IsFaulted))?.ToArray();
+
+                    foreach (Task t in tasksCopy)
+                    {
+                        tasks.Remove(t);
+                    }
+                }
+            });
+            return;
+        }
+    }
+}
