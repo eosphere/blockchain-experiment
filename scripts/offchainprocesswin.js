98a2abae84a78d5bfb8bfc5c74bfdb800b738208
SaradaChelluboyena <sarada.shasha@gmail.com>
2019-10-11 08:24:05 +1000

    Erase the ticket table dat based on limit
    Erase the ticket table dat based on limit

--- scripts/offchainprocesswin.js
+++ scripts/offchainprocesswin.js
@@ -10,7 +10,7 @@ const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
 const rpc = new JsonRpc("https://hub.area240.com", { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
-const drawno  = 0;
+const drawno  = 2;
 const account = "experiment13";
 var winnumbers = Array(6);
 
@@ -24,18 +24,54 @@ async function getTickets () {
         "table": "tickets", 
         "key_type": "i64",
         "index_position":3,
-        "lower_bound":drawno,
-        "upper_bound":drawno,
-        "limit": 10000,
-    })
+        "lower_bound" :drawno,
+        "upper_bound" :drawno,
+        "limit": 15000,
+    });
+    console.log ("No of tickets: "+ tickets.rows.length);
+ 
     tickets.rows.forEach(async function (item, index) {
-        
+        //console.log ("No win for ticket : "+ JSON.stringify(item));
+        if(item.ticket_status == 0) {
         var res = winnumbers.filter(f => item.entrynumbers.includes(f));
         if(res.length >= 4)
-        console.log ("Won!!! : "+item.entrynumbers + " div : "+((6-res.length+1)));
-        else
-        console.log ("No win for ticket : "+item.serialno +" entry no : "+item.entrynumbers);
+        updateTicket(item.serialno,(6-res.length+1));
+        }
         
+    });
+}
+
+async function updateTicket (ticketno,division) {
+
+    api.transact(
+        {
+        actions: [
+            {
+            account: account,
+            name: "updatewint",
+            authorization: [
+                {
+                actor: account,
+                permission: "active"
+                }
+            ],
+            data: {
+                serial_no: ticketno,
+                win_tier: division,
+             }
+            }
+        ]
+        },
+        {
+        blocksBehind: 3,
+        expireSeconds: 30
+        }
+    )
+    .then(function(result) {
+      console.log ("Ticket no : "+serialno+" Won!!! -  Div : "+division);
+    })
+    .catch(function(e) {
+        console.log(e);
     });
 }
 
