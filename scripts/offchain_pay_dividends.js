8f485792a8981fe3767b282f62cfc3fb7c18d212
pocadmin <pocadmin@desktop005.area240.com>
2019-10-14 10:29:53 +1000

    off chain processor: pay dividends

--- scripts/offchain_pay_dividends.js
+++ scripts/offchain_pay_dividends.js
@@ -3,20 +3,20 @@ const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
 const fetch = require('node-fetch');                                    // node only; not needed in browsers
 const { TextEncoder, TextDecoder } = require('util');                
 
-const defaultPrivateKey = "5Ke3bVZBoyixVhhFtXUNPHn1fiDAYsPdi7L9CcBmL5aQrUCrGhN"; // bob
+const defaultPrivateKey = "5JNQzM2iBr9ZhV9DSAoYPjMSn7KxVYSGLRFNFjXwP9FKU34UDZ6"; // bob
 const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
 
-const rpc = new JsonRpc("http://vsemppoceosbp1.area240.com:8888", { fetch });
+//const rpc = new JsonRpc("http://vsemppoceosbp1.area240.com:8888", { fetch });
+const rpc = new JsonRpc("http://localhost:8888", { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
-const drawno  = 2;
-const account = "experiment13";
+const drawno  = 1;
+const account = "experiment11";
 const min_winning_tier = 1;
-const max_winning_tier = 4;
+const max_winning_tier = 3;
 const winning_tier_index = 5;
-var dividends_map;
 
-async function getTickets() {
+async function processTickets() {
 
     const tickets = await rpc.get_table_rows({
         "json": true,
@@ -24,39 +24,56 @@ async function getTickets() {
         "scope": account, 
         "table": "tickets", 
         "key_type": "i64",
-        "index_position":winning_tier_index,
-        "lower_bound":min_winning_tier,
-        "upper_bound":max_winning_tier,
+        "index_position": winning_tier_index,
+        "lower_bound": min_winning_tier,
+        "upper_bound": max_winning_tier,
         "limit": 10000,
     })
     tickets.rows.forEach(async function (item, index) {
-        if (item.drawnumber != drawno || ticket_status != 0 || storeid != null) {
-            continue;
+        if (item.drawnumber != drawno || item.ticket_status != 0 || item.storeid > 0) {
+            console.log("tickets ignored:" + JSON.stringify(item));
+            console.log("\n");
+        } else {
+            console.log("Pay dividends for :" + JSON.stringify(item));
+            //payDividend(item);
+            claim (item.serialno, item.purchaser);
         }
-        //pay dividends
-        console.log("Pay dividends for :" + JSON.stringify(item));
-        // var res = winnumbers.filter(f => item.entrynumbers.includes(f));
-        // if(res.length >= 4)
-        // console.log ("Won!!! : "+item.entrynumbers + " div : "+((6-res.length+1)));
-        // else
-        // console.log ("No win for ticket : "+item.serialno +" entry no : "+item.entrynumbers);
-        
     });
 }
 
-async function getDividends()  {
-    const dividends = await rpc.get_table_rows({
-        "json": true,
-        "code": account, 
-        "scope": account, 
-        "table": "dividends", 
-        "key_type": "i64",
-        "index_position":1,
-        "lower_bound":drawno,
-        "upper_bound":drawno,
-        "limit": 100,
-    });
-    console.log("Total " + dividends.rows.length + " for draw " + drawno);
+async function claim (serialno, buyer) {
 
-    dividends_map = dividends.rows[0];
+    api.transact(
+        {
+        actions: [
+            {
+            account: account,
+            name: "claim",
+            authorization: [
+                {
+                actor: account,
+                permission: "active"
+                }
+            ],
+            data: {
+                serial_no: serialno
+                }
+            }
+        ]
+        },
+        {
+        blocksBehind: 3,
+        expireSeconds: 30
+        }
+    )
+    .then(function(result) {
+        console.log (result);
+        console.log ("Success");
+    })
+    .catch(function(e) {
+        throw e;
+    });
+    
 }
+
+processTickets();
