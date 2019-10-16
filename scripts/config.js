a5f39e00e6334675c08e8c243d24eadb04301b5a
Alok Mehrotra <Alok.Mehrotra@tattsgroup.com>
2019-10-16 21:22:42 +1000

    paging+cleaned code

--- scripts/config.js
+++ scripts/config.js
@@ -4,19 +4,17 @@ const fetch = require('node-fetch');                                    // node 
 const { TextEncoder, TextDecoder } = require('util');  
 
 global.crypto = require ('crypto');          
-global.defaultPrivateKey = "5JNQzM2iBr9ZhV9DSAoYPjMSn7KxVYSGLRFNFjXwP9FKU34UDZ6"; // experiment13
-//"5Ke3bVZBoyixVhhFtXUNPHn1fiDAYsPdi7L9CcBmL5aQrUCrGhN"; // experiment13
-//5JNQzM2iBr9ZhV9DSAoYPjMSn7KxVYSGLRFNFjXwP9FKU34UDZ6 //local
+global.defaultPrivateKey = "5KeWBLp3CAQwLZ8nccSBU7dXmz4axxATz7uZEN8K58qAQ2izjRd"; 
+
 global.signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
-global.url = "http://localhost:8888";
-//http://vsemppoceosbp1.area240.com:8888
-//http://localhost:8888
+global.url = "https://hub.area240.com";
+
 global.rpc = new JsonRpc(url, { fetch });
 global.api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
 global.range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 
-global.drawno  = 1;
+global.drawno  = 10;
 global.account = "experiment13";
 global.min_winning_tier = 1;
 global.max_winning_tier = 4;
